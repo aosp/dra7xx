d2ee35d72853e8c91ac45b50863dc54c9e58cc32
Sunita Nadampalli <sunitan@ti.com>
2014-11-14 12:11:35 -0600

    OMX: Added stagefright adaptation layer
    Change-Id: I3e5f4e3e53d1d357038435a035d76150d99a3661
    Signed-off-by: Sunita Nadampalli <sunitan@ti.com>

---
+++ omx/libstagefrighthw/Android.mk
@@ -0,0 +1,22 @@
+LOCAL_PATH := $(call my-dir)
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES := \
+    TIOMXPlugin.cpp
+
+LOCAL_C_INCLUDES:= \
+        $(TOP)/frameworks/native/include/media/openmax \
+        $(TOP)/frameworks/native/include/media/hardware
+
+LOCAL_SHARED_LIBRARIES :=       \
+        libbinder               \
+        libutils                \
+        libcutils               \
+        liblog                  \
+        libui                   \
+        libdl                   \
+
+LOCAL_MODULE := libstagefrighthw
+
+include $(BUILD_SHARED_LIBRARY)
+
